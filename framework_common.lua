c3591776e484208ecb1afc6249ac15139131a555
IllidanS4 <illidans4@gmail.com>
2021-06-30 17:11:31 +0200

    unpack command arguments

--- framework_common.lua
+++ framework_common.lua
@@ -6,8 +6,13 @@ local cor_resume = cor.resume
 local cor_yield = cor.yield
 local error = _ENV.error
 local rawset = _ENV.rawset
+local t_unpack_orig = table.unpack
 
 local Cfx_SetTimeout = Citizen.SetTimeout
+
+local function t_unpack(t)
+  return t_unpack_orig(t, 1, t.n)
+end
 
 -- async processing
 
@@ -72,14 +77,12 @@ local registered_commands = {}
 local function cmd_newindex(restricted)
   return function(self, key, value)
     if not registered_commands[key] then
-      local handler = function(...)
+      registered_commands[key] = RegisterCommand(key, function(source, args, rawCommand)
         local func = self[key]
         if func then
-          return FW_Async(func, ...)
+          return FW_Async(func, source, rawCommand, t_unpack(args))
         end
-      end
-      RegisterCommand(key, handler, restricted)
-      registered_commands[key] = handler
+      end, restricted)
     end
     return rawset(self, key, value)
   end
