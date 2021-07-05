b9d78160dcc129d9f25717602672d042307020ac
IllidanS4 <illidans4@gmail.com>
2021-07-05 20:03:58 +0200

    add source

--- server_callbacks.lua
+++ server_callbacks.lua
@@ -6,7 +6,7 @@ FW_RegisterCallback('OnEntityCreating', 'entityCreating', nil, true)
 FW_RegisterCallback('OnResourceStart', 'onResourceStart')
 FW_RegisterServerCallback('OnPlayerConnect', 'fivework:PlayerActivated', true)
 FW_RegisterCallback('OnPlayerDisconnect', 'playerDropped', true)
-FW_RegisterCallback('OnIncomingConnection', 'playerConnecting', nil, true)
+FW_RegisterCallback('OnIncomingConnection', 'playerConnecting', true, true)
 FW_RegisterServerCallback('OnPlayerEnterVehicle', 'baseevents:enteredVehicle', true, nil, function(source, localid, seat, modelkey, networkid, ...)
   return source, NetworkGetEntityFromNetworkId(networkid), seat, networkid, localid, modelkey, ...
 end)
