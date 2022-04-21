01e89c86c315fbde54a53367a57f25ec2e107630
IllidanS4 <illidans4@gmail.com>
2022-04-22 01:56:27 +0200

    fixed combinator name

--- framework_server.lua
+++ framework_server.lua
@@ -240,7 +240,7 @@ do
   end
   
   local function owner_scheduler_factory_no_wait(name)
-    name = name..'NetworkedArgument'
+    name = name..'NetworkIdIn1'
     return function(callback, entity, ...)
       local player = NetworkGetEntityOwner(entity)
       if not player then return callback(false) end
