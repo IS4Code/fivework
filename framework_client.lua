9b3b3918306b6ae71c5caa933650f15028670d6d
IS4 <illidans4@gmail.com>
2023-09-08 14:45:41 +0200

    fix arguments passed

--- framework_client.lua
+++ framework_client.lua
@@ -288,7 +288,7 @@ do
   local FW_TriggerCallback = _ENV.FW_TriggerCallback
   
   function FW_TriggerNetCallback(name, ...)
-    if FW_TriggerCallback('Before'..name) then
+    if FW_TriggerCallback('Before'..name, ...) then
       return
     end
     return t_insert(callbacks_queue, {name, observe_state(t_pack(...))})
